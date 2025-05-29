1a6b18594f367c734d8010a825441fd98c162acf
Lincoln-Dupree <lincoln.vfx@gmail.com>
2025-05-29 17:52:36 -0500

    Adding a directory for smaller code exercises thrughout the lessons

---
+++ javascript-exercises/00_random_stuff/random.js
@@ -0,0 +1,51 @@
+// "use strict";
+
+
+// // *** FUNCTION BASICS couple of examples at the end *** 
+
+
+// // Write a function called add7 that takes 
+// // one number and returns that number + 7.
+
+// function add7(number){
+//     number += 7;
+//     console.log(number);
+//     return number;
+// }
+
+// add7(14);
+
+// // Write a function called multiply that takes 
+// // 2 numbers and returns their product.
+
+// function multiply(num1, num2){
+//     let result = num1 * num2; 
+//     console.log(result);
+//     return result;
+// }
+
+// multiply(5,5);
+
+// // Write a function called capitalize 
+// // that takes a string and returns that string with only the first letter capitalized. 
+// // Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+
+// function capitalize(phrase){
+//     let result = String(phrase).charAt(0).toUpperCase() + String(phrase).slice(1);
+//     console.log(result);
+//     return result;
+// }
+
+// capitalize("hey there!");
+
+// // Write a function called lastLetter that takes 
+// // a string and returns the very last letter of that string:
+
+// function lastLetter(string){
+//     let result = String(string).slice(-1);
+//     console.log(result)
+//     return result;
+// }
+
+// lastLetter('ABCD');
+
